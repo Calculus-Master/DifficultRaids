3a579a1156092fa06095d1e52498af1ae1b39f51
Saptarshi Mallick <sapmallick@gmail.com>
2022-03-27 09:48:50 -0700

    Edited Illusioner Blindness Spell cast time and intervals

---
+++ src/main/java/com/calculusmaster/difficultraids/mixins/IllusionerBlindnessSpellGoalMixin.java
@@ -0,0 +1,22 @@
+package com.calculusmaster.difficultraids.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(targets = "net.minecraft.world.entity.monster.Illusioner$IllusionerBlindnessSpellGoal")
+public class IllusionerBlindnessSpellGoalMixin
+{
+    @Inject(at = @At("HEAD"), method = "getCastingTime", cancellable = true)
+    private void difficultraids_IllusionerBlindnessSpellGoal_getCastingTime(CallbackInfoReturnable<Integer> callbackInfoReturnable)
+    {
+        callbackInfoReturnable.setReturnValue(60);
+    }
+
+    @Inject(at = @At("HEAD"), method = "getCastingInterval", cancellable = true)
+    private void difficultraids_IllusionerBlindnessSpellGoal_getCastingInterval(CallbackInfoReturnable<Integer> callbackInfoReturnable)
+    {
+        callbackInfoReturnable.setReturnValue(360);
+    }
+}
