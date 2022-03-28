d9d68eefb104ad6a1a8f305494970266d4c1b2cf
Saptarshi Mallick <sapmallick@gmail.com>
2022-03-28 16:03:03 -0700

    Increased Dart Illager spawn amounts in Raid Waves

--- src/main/java/com/calculusmaster/difficultraids/raids/RaiderSpawnRegistry.java
+++ src/main/java/com/calculusmaster/difficultraids/raids/RaiderSpawnRegistry.java
@@ -84,9 +84,9 @@ public class RaiderSpawnRegistry
 
         RaiderSpawnRegistry.createFor("DART_ILLAGER")
                 .withDifficulty(RaidDifficulty.DEFAULT,     new int[]{0, 0, 0, 0, 0, 1, 1, 2})
-                .withDifficulty(RaidDifficulty.HERO,        new int[]{0, 0, 0, 0, 1, 2, 2, 3})
-                .withDifficulty(RaidDifficulty.LEGEND,      new int[]{0, 1, 0, 0, 2, 3, 3, 3})
-                .withDifficulty(RaidDifficulty.MASTER,      new int[]{0, 2, 1, 1, 6, 6, 0, 8})
+                .withDifficulty(RaidDifficulty.HERO,        new int[]{0, 0, 1, 0, 1, 2, 2, 3})
+                .withDifficulty(RaidDifficulty.LEGEND,      new int[]{0, 1, 3, 1, 1, 3, 4, 5})
+                .withDifficulty(RaidDifficulty.MASTER,      new int[]{0, 3, 2, 1, 6, 6, 4, 8})
                 .withDifficulty(RaidDifficulty.APOCALYPSE,  new int[]{0, 8, 8, 10, 12, 16, 20, 30})
                 .register();
     }
